eb40a990c30bb61578c23d20ad90fc7e35a91c18
Jaad Tannous <jaad.tannous.t7@gmail.com>
2024-09-18 15:30:07 -0400

    removed xml links from spcoll.py and adjusted the xml.py in io

--- lvlspy/spcoll.py
+++ lvlspy/spcoll.py
@@ -61,52 +61,6 @@ class SpColl(lp.Properties):
 
         return self.spcoll
 
-    def write_to_xml(self, file, pretty_print=True, units="keV"):
-        """Link to IO to write the collection to XML.
-
-        Args:
-            ``file`` (:obj:`str`) The output file name.
-
-            ``pretty_print`` (:obj:`bool`, optional): If set to True,
-            routine outputs the xml in nice indented format.
-
-            ``units`` (:obj:`str`, optional): A string for the energy units.
-
-        Return:
-            On successful return, the species collection data have been
-            written to the XML output file.
-
-        """
-
-        lio.XML().write_to_xml(self, file, pretty_print, units)
-
-    def validate(self, file):
-        """Link to IO to validate a species collection XML file.
-
-        Args:
-            ``file`` (:obj:`str`) The name of the XML file to validate.
-
-        Returns:
-            An error message if invalid and nothing if valid.
-
-        """
-        lio.XML().validate(file)
-
-    def update_from_xml(self, file, xpath=""):
-        """Link to update a species collection from an XML file.
-
-        Args:
-            ``file`` (:obj:`str`) The name of the XML file from which to update.
-
-            ``xpath`` (:obj:`str`, optional): XPath expression to select
-            species.  Defaults to all species.
-
-        Returns:
-            On successful return, the species collection has been updated.
-
-        """
-        lio.XML().update_from_xml(self, file, xpath)
-
     def update_from_ensdf(self, file, sp):
         """Link to update a species collection from ENSDF file
 
