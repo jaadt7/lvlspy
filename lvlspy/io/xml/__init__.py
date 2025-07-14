4a83b6216714af2245ab3640619f51fbb2bfe580
Jaad Tannous <jaad.tannous.t7@gmail.com>
2025-07-14 15:15:07 -0400

    adjusted file name again

--- lvlspy/io/xml/__init__.py
+++ lvlspy/io/xml/__init__.py
@@ -3,10 +3,10 @@ A module to handle input and output of xml.
 """
 
 import os
-from . import _fxml
+from . import __xml
 
-__all__ = [name for name in dir(_fxml) if not name.startswith("_")]
-globals().update({name: getattr(_fxml, name) for name in __all__})
+__all__ = [name for name in dir(__xml) if not name.startswith("_")]
+globals().update({name: getattr(__xml, name) for name in __all__})
 
 xml_catalog = os.path.join(os.path.dirname(__file__), "xsd_pub/catalog")
 
