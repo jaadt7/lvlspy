c3cb41a95a52840c010bf4c1b94aad8bc7fbb744
Bradley Meyer <mbradle@clemson.edu>
2026-07-22 11:24:19 -0400

    Add core/extensions package skeletons for the planned layout (1)

--- lvlspy/__init__.py
+++ lvlspy/__init__.py
@@ -3,11 +3,7 @@ A package of python routines to handle quantum-level system data.
 """
 
 from lvlspy.__about__ import __version__
-from lvlspy.level import Level
-from lvlspy.properties import Properties
-from lvlspy.spcoll import SpColl
-from lvlspy.species import Species
-from lvlspy.transition import Transition
+from lvlspy.core import Level, Properties, SpColl, Species, Transition
 
 __all__ = [
     "__version__",
--- lvlspy/core/__init__.py
+++ lvlspy/core/__init__.py
@@ -1,2 +1,15 @@
 """Core domain model for lvlspy."""
 
+from lvlspy.core.level import Level
+from lvlspy.core.properties import Properties
+from lvlspy.core.spcoll import SpColl
+from lvlspy.core.species import Species
+from lvlspy.core.transition import Transition
+
+__all__ = [
+    "Level",
+    "Properties",
+    "SpColl",
+    "Species",
+    "Transition",
+]
