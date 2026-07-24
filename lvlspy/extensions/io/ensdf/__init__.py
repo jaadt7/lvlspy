722d0752e137f2f2c0862137a6567018099af569
Bradley Meyer <mbradle@clemson.edu>
2026-07-24 08:55:16 -0400

    Format with black.

--- lvlspy/extensions/io/ensdf/__init__.py
+++ lvlspy/extensions/io/ensdf/__init__.py
@@ -2,6 +2,18 @@
 
 # pylint: disable=duplicate-code
 
-from ._ensdf import update_from_ensdf, write_to_ensdf, update_reduced_matrix_coefficient,remove_undefined_levels,fill_missing_ensdf_transitions
+from ._ensdf import (
+    update_from_ensdf,
+    write_to_ensdf,
+    update_reduced_matrix_coefficient,
+    remove_undefined_levels,
+    fill_missing_ensdf_transitions,
+)
 
-__all__ = ["update_from_ensdf", "write_to_ensdf","update_reduced_matrix_coefficient","remove_undefined_levels","fill_missing_ensdf_transitions"]
+__all__ = [
+    "update_from_ensdf",
+    "write_to_ensdf",
+    "update_reduced_matrix_coefficient",
+    "remove_undefined_levels",
+    "fill_missing_ensdf_transitions",
+]
